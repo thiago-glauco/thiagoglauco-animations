72a7d589b763cb89177c4da5f115da83acbf0ceb
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-06-03 09:09:35 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow',
+      })
+      ),
+      state('close', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+  ])]
 })
 export class AppComponent  {
   name = 'Angular';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
+//Very inicial Setup
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import 'hammerjs';
+
 
 @NgModule({
   imports:      [ 
