01fbe2bbdc85952c2ce696ab62df75120d3b9d90
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-06-03 08:39:45 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import 'hammerjs';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
